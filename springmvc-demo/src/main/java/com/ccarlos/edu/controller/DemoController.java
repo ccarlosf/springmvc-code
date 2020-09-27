462da363e8234d813cb082f236a4ee750f8cb42d
ccarlosf <2238513100@qq.com>
2020-09-27 11:26:06 +0800

    01 第一阶段 开源框架源码剖析 SpringMVC 高级框架 第一部分 SPringMVC 应用 第3节 请求参数绑定

--- springmvc-demo/src/main/java/com/ccarlos/edu/controller/DemoController.java
+++ springmvc-demo/src/main/java/com/ccarlos/edu/controller/DemoController.java
@@ -1,15 +1,35 @@
 package com.ccarlos.edu.controller;
 
+import com.ccarlos.edu.pojo.QueryVo;
+import com.ccarlos.edu.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Date;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+
+    // SpringMVC的异常处理机制（异常处理器）
+    // 注意：写在这里只会对当前controller类生效
+    @ExceptionHandler(ArithmeticException.class)
+    public void handleException(ArithmeticException exception,HttpServletResponse response) {
+        // 异常处理逻辑
+        try {
+            response.getWriter().write(exception.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * url: http://localhost:8080/demo/handle01
@@ -17,7 +37,7 @@ public class DemoController {
     @RequestMapping("/handle01")
     public ModelAndView handle01(@ModelAttribute("name") String name) {
 
-//        int c = 1/0;
+        int c = 1/0;
 
 
         Date date = new Date();// 服务器时间
@@ -25,8 +45,87 @@ public class DemoController {
         // 封装了数据和页面信息的 ModelAndView
         ModelAndView modelAndView = new ModelAndView();
         // addObject 其实是向请求域中request.setAttribute("date",date);
-        modelAndView.addObject("date",date);
+        modelAndView.addObject("date", date);
         // 视图信息(封装跳转的页面信息) 逻辑视图名
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    /**
+     * SpringMVC 对原生servlet api的支持  url：/demo/handle02?id=1
+     * <p>
+     * 如果要在SpringMVC中使用servlet原生对象，比如HttpServletRequest\HttpServletResponse\HttpSession，直接在Handler方法形参中声明使用即可
+     */
+    @RequestMapping("/handle02")
+    public ModelAndView handle02(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        String id = request.getParameter("id");
+
+        Date date = new Date();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("date", date);
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    /*
+     * SpringMVC 接收简单数据类型参数  url：/demo/handle03?ids=1
+     *
+     * 注意：接收简单数据类型参数，直接在handler方法的形参中声明即可，框架会取出参数值然后绑定到对应参数上
+     * 要求：传递的参数名和声明的形参名称保持一致
+     */
+    @RequestMapping("/handle03")
+    public ModelAndView handle03(@RequestParam("ids") Integer id, Boolean flag) {
+
+        Date date = new Date();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("date", date);
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    /*
+     * SpringMVC接收pojo类型参数  url：/demo/handle04?id=1&username=zhangsan
+     *
+     * 接收pojo类型参数，直接形参声明即可，类型就是Pojo的类型，形参名无所谓
+     * 但是要求传递的参数名必须和Pojo的属性名保持一致
+     */
+    @RequestMapping("/handle04")
+    public ModelAndView handle04(User user) {
+
+        Date date = new Date();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("date", date);
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    /*
+     * SpringMVC接收pojo包装类型参数  url：/demo/handle05?phone=13132345123&mail=32131@qq.com&user.id=1&user.name=zhangsan
+     * 不管包装Pojo与否，它首先是一个pojo，那么就可以按照上述pojo的要求来
+     * 1、绑定时候直接形参声明即可
+     * 2、传参参数名和pojo属性保持一致，如果不能够定位数据项，那么通过属性名 + "." 的方式进一步锁定数据
+     *
+     */
+    @RequestMapping("/handle05")
+    public ModelAndView handle05(QueryVo queryVo) {
+
+        Date date = new Date();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("date",date);
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    /**
+     * 绑定日期类型参数
+     * 定义一个SpringMVC的类型转换器  接口，扩展实现接口接口，注册你的实现
+     * @param birthday
+     * @return
+     */
+    @RequestMapping("/handle06")
+    public ModelAndView handle06(Date birthday) {
+        Date date = new Date();ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("date",date);
         modelAndView.setViewName("success");
         return modelAndView;
     }
